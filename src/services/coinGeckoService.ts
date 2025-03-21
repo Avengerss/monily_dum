c4a10175bdaf4f36343cd322df222ee12b30efdd
Avengerss <usemonily@gmail.com>
2025-03-21 13:59:29 +0100

    Update coinGeckoService.ts

--- src/services/coinGeckoService.ts
+++ src/services/coinGeckoService.ts
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
 const API_URL = 'https://api.coingecko.com/api/v3/simple/price';
+const API_MARKET_URL = 'https://api.coingecko.com/api/v3/coins/markets';
 
 type CryptoSymbols = 'bitcoin' | 'ethereum' | 'tether' | 'binancecoin' | 'solana' | 'cardano' | 'polkadot' | 'ripple' | 'dogecoin' | 'litecoin' | 'chainlink';
-type FiatCurrencies = 'ngn' | 'zar' | 'kes' | 'ghs' | 'egp' | 'mad';
+type FiatCurrencies = 'ngn' | 'zar' | 'kes' | 'ghs' | 'egp' | 'mad' | 'usd';
 
 interface CryptoRates {
     [key: string]: { [currency: string]: number };
 }
+
+interface CryptoMarketData {
+    id: string;
+    current_price: number;
+    price_change_percentage_24h: number;
+    total_volume: number;
+    market_cap: number;
+}
+
 
 export const getCryptoRates = async (currency: FiatCurrencies): Promise<CryptoRates | null> => {
     try {
@@ -30,4 +40,23 @@ export const getAvailableCurrencies = (): FiatCurrencies[] => {
 
 export const getAvailableCryptos = (): CryptoSymbols[] => {
     return ['bitcoin', 'ethereum', 'tether', 'binancecoin', 'solana', 'cardano','polkadot','ripple','dogecoin','litecoin','chainlink'];
+};
+
+export const getCryptoMarketData = async (currency: FiatCurrencies): Promise<CryptoMarketData[] | null> => {
+    try {
+        const response = await axios.get<CryptoMarketData[]>(API_MARKET_URL, {
+            params: {
+                vs_currency: currency,
+                ids: 'bitcoin,ethereum,tether,binancecoin,solana,cardano,polkadot,ripple,dogecoin,litecoin,chainlink,sui',
+                order: 'market_cap_desc',
+                per_page: 12,
+                page: 1,
+                sparkline: false
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching crypto market data:', error);
+        return null;
+    }
 };
